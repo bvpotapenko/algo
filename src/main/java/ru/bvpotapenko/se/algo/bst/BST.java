85dcabce264c4edf2b6b86eb6b71e341089e6dd1
bvpotapenko <bvpotapenko@gmail.com>
2019-02-15 14:45:58 +0800

    BST: 1. isEmpty() 2. size(), size(Node) 3. get(Key), get(Node, Key) 4. put(Key, Value), put(Node, Key, Value) 5. contains(Value), containsValue(Node, Value)

--- src/main/java/ru/bvpotapenko/se/algo/bst/BST.java
+++ src/main/java/ru/bvpotapenko/se/algo/bst/BST.java
@@ -1,65 +1,92 @@
 package ru.bvpotapenko.se.algo.bst;
 
 public class BST<Key extends Comparable<Key>, Value> {
-    private class Node{
+    private class Node {
         Key key;
         Value value;
         Node left;
         Node right;
         int size; //Amount of nodes in the tree that grows from current node
 
-        public Node(Key key, Value value, int size){
+        public Node(Key key, Value value, int size) {
             this.key = key;
             this.value = value;
             this.size = size;
         }
     }
+
     private Node root = null;
 
-    public boolean isEmpty(){return root == null;}
-    public int size(){return size(root);}
+    public boolean isEmpty() {
+        return root == null;
+    }
 
-    private int size(Node node){
-        if(node == null)
+    public int size() {
+        return size(root);
+    }
+
+    private int size(Node node) {
+        if (node == null)
             return 0;
         else
             return node.size;
     }
-    public Value get(Key key){
+
+    public Value get(Key key) {
         return get(root, key);
     }
-    private Value get(Node node, Key key){
-        if(key == null) throw new IllegalArgumentException("Null key");
-        if(node == null) return null;
+
+    private Value get(Node node, Key key) {
+        if (key == null) throw new IllegalArgumentException("Null key");
+        if (node == null) return null;
         final int cmp = key.compareTo(node.key);
-        if(cmp == 0){
+        if (cmp == 0) {
             return node.value;
-        }else if(cmp < 0){
+        } else if (cmp < 0) {
             return get(node.left, key);
-        }else{ //cmp > 0)
+        } else { //cmp > 0)
             return get(node.right, key);
         }
     }
-    public boolean containsKey(Key key){return get(key) != null;}
-    public boolean containsValue(Value value){return false;} // TODO: 15-Feb-19
 
-    private Node put(Node node, Key key, Value value){
-        if(node == null){
+    public boolean containsKey(Key key) {
+        return get(key) != null;
+    }
+
+
+    public boolean containsValue(Value value) {
+        return containsValue(root, value);
+    }
+
+    private boolean containsValue(Node node, Value value) {
+        if (node == null) return false;
+        if (node.value.equals(value)) {
+            return true;
+        }
+        if (containsValue(node.left, value)) {
+            return true;
+        }
+        return containsValue(node.right, value);
+    }
+
+    private Node put(Node node, Key key, Value value) {
+        if (node == null) {
             return new Node(key, value, 1);
         }
         int cmp = key.compareTo(node.key);
-        if (cmp == 0){
+        if (cmp == 0) {
             node.value = value;
-        }else if(cmp < 0){
+        } else if (cmp < 0) {
             node.left = put(node.left, key, value);
-        }else { //cmp > 0
+        } else { //cmp > 0
             node.right = put(node.right, key, value);
         }
         node.size = size(node.left) + size(node.right) + 1;
         return node;
     }
-    public void put(Key key, Value value){
-        if(key == null) throw new IllegalArgumentException("Key can't be null");
+
+    public void put(Key key, Value value) {
+        if (key == null) throw new IllegalArgumentException("Key can't be null");
         root = put(root, key, value);
     }
 }
