4a428311337442fed855047f4a4eb91fe0e01dc8
bvpotapenko <bvpotapenko@gmail.com>
2019-02-15 19:52:44 +0800

    BST: 1. isEmpty() 2. size(), size(Node) 3. get(Key), get(Node, Key) 4. put(Key, Value), put(Node, Key, Value) 5. contains(Value), containsValue(Node, Value) 6. delete() 7. toString()

--- src/main/java/ru/bvpotapenko/se/algo/bst/BST.java
+++ src/main/java/ru/bvpotapenko/se/algo/bst/BST.java
@@ -1,5 +1,7 @@
 package ru.bvpotapenko.se.algo.bst;
 
+import java.util.NoSuchElementException;
+
 public class BST<Key extends Comparable<Key>, Value> {
     private class Node {
         Key key;
@@ -7,11 +9,15 @@ public class BST<Key extends Comparable<Key>, Value> {
         Node left;
         Node right;
         int size; //Amount of nodes in the tree that grows from current node
+        int height; //Distance from current node to it's farthest leaf;
+        int level; //This node level from root. Is used for HW.
 
-        public Node(Key key, Value value, int size) {
+        public Node(Key key, Value value, int size, int height, int level) {
             this.key = key;
             this.value = value;
             this.size = size;
+            this.height = height;
+            this.level = level;
         }
     }
 
@@ -53,7 +59,6 @@ public class BST<Key extends Comparable<Key>, Value> {
         return get(key) != null;
     }
 
-
     public boolean containsValue(Value value) {
         return containsValue(root, value);
     }
@@ -69,24 +74,191 @@ public class BST<Key extends Comparable<Key>, Value> {
         return containsValue(node.right, value);
     }
 
-    private Node put(Node node, Key key, Value value) {
+    private Node put(Node node, Key key, Value value, int level) {
         if (node == null) {
-            return new Node(key, value, 1);
+            return new Node(key, value, 1, 0, level);
         }
         int cmp = key.compareTo(node.key);
         if (cmp == 0) {
             node.value = value;
         } else if (cmp < 0) {
-            node.left = put(node.left, key, value);
+            node.left = put(node.left, key, value, node.level + 1);
         } else { //cmp > 0
-            node.right = put(node.right, key, value);
+            node.right = put(node.right, key, value, node.level + 1);
         }
         node.size = size(node.left) + size(node.right) + 1;
+        node.height = Math.max(height(node.left), height(node.right)) + 1;
         return node;
     }
 
     public void put(Key key, Value value) {
         if (key == null) throw new IllegalArgumentException("Key can't be null");
-        root = put(root, key, value);
+        root = put(root, key, value, 0);
+    }
+
+    private Node min(Node node){
+        if(node.left == null)
+            return node;
+        else
+            return min(node.left);
+    }
+
+    private Node max(Node node){
+        if(node.right == null)
+            return node;
+        else
+            return max(node.right);
+    }
+
+    private Node deleteMin(Node node){
+        if (node.left == null){
+            return node.right;
+        }else{
+            node.left = deleteMin(node.left);
+        }
+        return node;
+    }
+    public void deleteMin(){
+        if(isEmpty())
+            throw new NoSuchElementException("Tree is empty");
+        root = deleteMin(root);
+    }
+
+    private Node deleteMax(Node node){
+        if (node.right == null){
+            return node.left;
+        }else{
+            node.right = deleteMax(node.left);
+        }
+        return node;
+    }
+    public void deleteMax(){
+        if(isEmpty())
+            throw new NoSuchElementException("Tree is empty");
+        root = deleteMax(root);
+    }
+
+    private Node delete(Node node, Key key){
+        if (node == null) {
+            return null;
+        }
+        int cmp = key.compareTo(node.key);
+        if(cmp < 0){
+            node.left = delete(node.left, key);
+        }else if (cmp > 0){
+            node.right = delete(node.right, key);
+        }else{
+             if (node.left == null) {
+                 return node.right;
+             }
+            if (node.right == null) {
+                return node.left;
+            }
+            Node tmp = node;
+            node = max(node.left);
+            node.left = deleteMax(node.left);
+            node.right = tmp.right;
+            tmp = null;
+        }
+        node.size = size(node.left) + size(node.right) + 1;
+        node.height = isLeaf(node) ? 0 : Math.max(height(node.left), height(node.right)) + 1;
+        return node;
+    }
+
+    public void delete(Key key){
+        root = delete(root, key);
+    }
+
+    public int height(){
+        if(isEmpty())
+            return 0;
+        return height(root);
+    }
+    private int height(Node node){
+        if(node == null) return 0;
+        return node.height;
+    }
+
+    //Counts height for a given node
+    private int countHeight(Node node){
+        if (isLeaf(node)) return 0;
+        int leftHeight = 0;
+        int rightHeight = 0;
+        if(node.left != null)
+            leftHeight = 1 + countHeight(node.left);
+        if(node.right != null)
+            rightHeight = 1 + countHeight(node.right);
+        return leftHeight > rightHeight ? leftHeight : rightHeight;
+    }
+
+    private boolean isLeaf(Node node){
+        if(node.left == null && node.right == null)
+            return true;
+        return false;
+    }
+
+    public boolean isBalanced(){
+        //Empty tree is balanced
+        if(isEmpty()) return true;
+        return isBalanced(root);
+    }
+    private boolean isBalanced(Node node){
+        if(isLeaf(node)) return true;
+        //With height 0 or 1 still balanced
+        if(height(node) < 2) return true;
+        //Height is gt 1 and one of subtrees is null means not balanced
+        if(node.left == null || node.right == null)
+            return false;
+        //Left and right subtrees exist and their heights differ more then 1
+        if(isBalanced(node.left) &&
+                isBalanced(node.right) &&
+                height(root.left) - height(root.right) <= 1){
+            return true;
+        }else
+            return false;
+    }
+
+    public String toString(){
+        if(isEmpty()) return "";
+        return subTreeToString(root, false, root.left != null);
+    }
+    private String subTreeToString(Node node, boolean isRightBranch, boolean parentHasLeft){
+        parentHasLeft = parentHasLeft || node.left != null;
+        String tree = "["+ getFiller(" ", 2 - node.key.toString().length())
+                + node.key + "]"; //"; " + node.value+"]";
+        if(node.right != null){
+            tree = tree + "--" + subTreeToString(node.right, true, parentHasLeft);
+        }
+        String spacing = "";
+        String leftSibling = "";
+        if(parentHasLeft) leftSibling = "|";
+        if(isRightBranch) spacing = leftSibling + getFiller("       ", node.level - 1) + "|\n" +
+                leftSibling + getFiller("       ", node.level - 1);
+
+        if(node.left != null){
+            tree = tree + "\n" +
+                    (spacing.isEmpty() ? leftSibling+"\n" : spacing) +
+                    subTreeToString(node.left, false, parentHasLeft);
+        }
+        return tree;
+    }
+
+    private String getFiller(String filler, int n){
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i <= n; i++)
+            sb.append(filler);
+        return sb.toString();
+    }
+
+    public String simpleToString(){
+        if(isEmpty()) return "";
+        return simpleToString(root, 0);
+    }
+    private String simpleToString(Node node, int marker){
+        if (node == null) return "";
+        String tree =
+                "[" +node.key + "; " + node.value + "] -- ("+marker + simpleToString(node.right, marker+1) +
+                "\n" + simpleToString(node.left, marker + 1) + marker+")";
+        return tree;
     }
 }
